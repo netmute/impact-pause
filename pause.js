bb8755c160cff746e555e64edb284ae1db112e2c
Benoit Asselin <contact@ab-d.fr>
2014-02-17 12:10:21 +0100

    Update pause.js
    tab to 2 spaces

--- pause.js
+++ pause.js
@@ -1,47 +1,47 @@
 ig.module(
-	'plugins.pause'
+  'plugins.pause'
 )
 .requires(
-	'impact.game',
-	'impact.timer'
+  'impact.game',
+  'impact.timer'
 )
 .defines(function() { "use strict";
 
 ig.Game.inject({
-	
-	paused: false,
-	pauseDelayTimer: new ig.Timer(),
-	pauseButtonDelay: .2,
-	
-	updateEntities: function() {
-		var i, ent;
-		for( i = 0; ent = this.entities[i]; ++i ) {
-			if( !ent._killed && !this.paused ) {
-				ent.update();
-			} else if( ent.ignorePause ) {
-				ent.update();
-			}
-		}
-	},
-	
-	togglePause: function(override) {
-		if( this.pauseDelayTimer.delta() > this.pauseButtonDelay ) {
-			this.paused = null != override ? override : !this.paused;
-			if( !this.paused ) {
-				this.onResume();
-			} else {
-				this.onPause();
-			}
-			this.pauseDelayTimer.reset();
-		}
-	},
-	
-	onResume: function() { },
-	onPause: function() { }
+  
+  paused: false,
+  pauseDelayTimer: new ig.Timer(),
+  pauseButtonDelay: .2,
+  
+  updateEntities: function() {
+    var i, ent;
+    for( i = 0; ent = this.entities[i]; ++i ) {
+      if( !ent._killed && !this.paused ) {
+        ent.update();
+      } else if( ent.ignorePause ) {
+        ent.update();
+      }
+    }
+  },
+  
+  togglePause: function(override) {
+    if( this.pauseDelayTimer.delta() > this.pauseButtonDelay ) {
+      this.paused = null != override ? override : !this.paused;
+      if( !this.paused ) {
+        this.onResume();
+      } else {
+        this.onPause();
+      }
+      this.pauseDelayTimer.reset();
+    }
+  },
+  
+  onResume: function() { },
+  onPause: function() { }
 });
 
 ig.Entity.inject({
-	ignorePause: false
+  ignorePause: false
 });
 
 });
